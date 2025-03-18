f1ba1c505b14b397bcb9fcb583bb3a8c3e7f58af
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-03-18 06:33:33 +0300

    Add DOMContentLoaded event listener and test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// Wait for the DOMContentLoaded event
+document.addEventListener("DOMContentLoaded", function() {
+    // When the DOM is fully loaded, change the text content of the paragraph
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
+  // This will log to the console before the DOM is fully loaded
+  console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+  
